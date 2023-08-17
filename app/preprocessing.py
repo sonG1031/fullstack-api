65389e0d53894330146bed8c33e0c30a7e545501
Minsub Song <minsub1031@gmail.com>
2023-08-17 12:12:32 +0900

    판다스로 데이터 전처리

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import requests
 
+def add_url(row):
+    return f'https://www.imdb.com/title/tt{row}/'
+
+def add_rating(df):
+    ratings_df = pd.read_csv('data/ratings.csv')
+    ratings_df['movieId'] = ratings_df['movieId'].astype(str)
+    agg_df = ratings_df.groupby('movieId').agg(
+        ratings_count = ('rating', 'count'),
+        ratings_avg = ('rating', 'mean')
+    ).reset_index()
+    
+    ratings_added_df = df.merge(agg_df, on='movieId')
+    return ratings_added_df
 
 if __name__ == "__main__":
     movies_df = pd.read_csv("data/movies.csv")
@@ -8,4 +21,7 @@ if __name__ == "__main__":
     
     links_df = pd.read_csv('data/links.csv', dtype=str)
     merged_df = movies_df.merge(links_df, on='movieId', how="left")
-    print(merged_df)
+    merged_df['url'] = merged_df['imdbId'].apply(lambda x: add_url(x))
+    
+    result_df = add_rating(merged_df)
+    print(result_df)
